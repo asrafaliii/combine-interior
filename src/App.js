b28f882ce5f1a9ec867632058a8d26ac7039a99d
Asraf Ali <asraf.bookaand@gmail.com>
2022-06-05 23:30:26 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,69 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return <div></div>;
+  const navbarItem = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Team</a>
+      </li>
+      <li>
+        <a>Services</a>
+      </li>
+      <li>
+        <a>Portfolio</a>
+      </li>
+      <li>
+        <a>Blogs</a>
+      </li>
+      <li>
+        <a>Contact</a>
+      </li>
+    </>
+  );
+  return (
+    <div class="navbar bg-base-100 px-12">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navbarItem}
+          </ul>
+        </div>
+        {/* <a class="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+        <a>
+          <img src={Logo} width={200} />
+        </a>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">{navbarItem}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
